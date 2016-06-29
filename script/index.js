f7c8747ed30f81dc37b80cc5f6950aac4d1523a8
youyidecai <578699990@qq.com>
2016-06-29 20:52:26 +0800

    插件化
    单纯的js，不依赖框架

--- script/index.js
+++ script/index.js
@@ -1,130 +1,128 @@
-(function() {
+window.onload = function() { (function() {
 
-	//自适应页面高度
-	var fixedHeight = 0;
-//	var headerHeight = $('header').height() || 0;
-	var headerHeight = 0;
-	var footerHeight = 0;
-	
-	//document.title = document.documentElement.clientHeight;
-	setTimeout(function(){
-		//延迟以便获取准确的高度，微信中默认打开webview获取的页面高度很高
-		var mHeight = document.documentElement.clientHeight - headerHeight;
-		$('.container').css('min-height',mHeight);
-	}, 100);
+        //自适应页面高度;
+        var headerHeight = 0;
 
-	$('#h5_vedio').height(document.documentElement.clientHeight - headerHeight);
-	var Media = document.getElementById("video1"); 
-		  var l = 0;
-		  $("#J_ctrl").on('click', function() {
-		  	if(Media.paused) {
-		  		Media.play();
-		  		$(this).removeClass().addClass("btn-control-play");
-		  		bartimer; 
-		  	}
-		  	else {
-		  		Media.pause();
-		  		$(this).removeClass().addClass("btn-control");
-		  		window.clearInterval(bartimer);  
-		  	}		  
-		  });
-		  function setProcess() {
-		  	var j_process_all = $('div.vedio-process');
-			  var j_process = $('.vedio-process-status');
-			  var j_process_dot = $('.vedio-process-dot');
-			  var vedioTime = Media.duration; //总时间
-			  var currentTime = Media.currentTime;
-			  /*var endTime = Media.*/
-		  	var j_preocess_width;
-		  	j_preocess_width = (currentTime/vedioTime) * j_process_all.width();
-		  	j_process.width(j_preocess_width);
-		  	j_process_dot.css("left", j_preocess_width);
+        setTimeout(function() {
+            //延迟以便获取准确的高度，微信中默认打开webview获取的页面高度很高
+            var mHeight = document.documentElement.clientHeight - headerHeight;
+            var $content = document.querySelector(".container");
 
-		  	$('#currentTime').text(formate_time(currentTime));
-		  	$('#allTime').text("/" + formate_time(vedioTime));
-		  	/*console.log(Media.duration);*/
+            $content.style.minHeight = mHeight + "px"; //css('min-height',mHeight);
+        },
+        100);
 
-		  	if(j_preocess_width == j_process_all.width()){  
-			    window.clearInterval(bartimer);  
-			  }  
-		  }
+        var $h5vedio = document.querySelector("#h5_vedio");
+        $h5vedio.style.height = document.documentElement.clientHeight - headerHeight + "px";
 
-		  var bartimer = window.setInterval(function(){setProcess();},100); 
-		  window.onload = function(){  
-			   
-			}  
+        //初始化操作-初始化时间显示
+        function setVedioTime() {
+            this.Media = document.getElementById("video1");
+            this.Jctrl = document.querySelector("#J_ctrl");
+            this.allTime = document.querySelector("#allTime");
+            this.currentTimeNode = document.querySelector("#currentTime");
 
-			$(".backtop p.btn-backtop").click(function(){
-				$('body, html').animate({scrollTop:0}, 500);
-				return false;
-			});
-			//
-			var v = function(e) {
-		          $(".vedio-process-status").css({
-		              width: e + 10
-		          }),
-		          $(".vedio-process-dot").css({
-		              left: e
-		          })
-		      }
-		  //计算时间
-		  function formate_time(ti) {
-		  	var t = "", 
-		  	    a = parseInt(ti), 
-		  	    _s = a % 60, 
-		  	    _m = parseInt(a / 60)   //minute, 
-		  	    _h = 0;
-	        return _m > 60 && (_h = parseInt(_m / 60),  //小时存在 _h = 小时数
-	        _m = parseInt(n % 60)),  //计算分钟数
-	        10 > _m && (_m = "0" + _m),  
-	        10 > _s && (_s = "0" + _s),
-	        t = _h ? _h + ":" + _m + ":" + _s : _m + ":" + _s
-		  }
+            //计算时间
+            this.formate_time = function(ti) {
+                var t = "",
+                a = parseInt(ti),
+                _s = a % 60,
+                _m = parseInt(a / 60); //minute, 
+                _h = 0;
+                return _m > 60 && (_h = parseInt(_m / 60), //小时存在 _h = 小时数
+                _m = parseInt(n % 60)),
+                //计算分钟数
+                10 > _m && (_m = "0" + _m),
+                10 > _s && (_s = "0" + _s),
+                t = _h ? _h + ":" + _m + ":" + _s: _m + ":" + _s;
+            };
 
-		  function moveStart(e) {
-		  }
-		  function moveIng(e) {
-		  	var process = $('div.vedio-process');
-		  	l;
-	        var X = e.touches[0].pageX, 
-	        	treasure = $(".vedio-process-dot");
-	        -5 > X ? X = -5 : X > process.width() - treasure.width() + 12 && (X = process.width() - treasure.width() + 12),
-	        l = X;
-	        var duration = parseInt(Media.duration), 
-	            time = duration * X / parseInt($('div.vedio-process').width());
-	        $("#currentTime").text(formate_time(time));
-	        v(X);
-		  }
-		  function moveEnd(e) {
-		  	var duration = parseInt(Media.duration), 
-		  		currentTime = duration * l / parseInt($('div.vedio-process').width());
-		  	Media.currentTime  = currentTime;
-		  }
-	  	document.querySelector(".vedio-process-dot").addEventListener("touchstart", moveStart),
-        document.querySelector(".vedio-process-dot").addEventListener("touchmove", moveIng),
-        document.querySelector(".vedio-process-dot").addEventListener("touchend", moveEnd);
-		  
-})();
+            //用于设置滑动滑块时的样式
+            this.setMovingDot = function(wt) {
+                this.j_process.style.width = wt + 10 + "px";
+                this.j_process_dot.style.left = wt + "px";
+            };
+        }
+        setVedioTime.prototype.initTime = function() {
+            this.currentTime = Number(this.Media.currentTime); //视频当前播放时间
+            this.vedioTime = Number(this.Media.duration); //视频总时间
+            this.allTime.firstChild.nodeValue = "/" + this.formate_time(this.vedioTime); //设置视频播放总时长
+            this.currentTimeNode.firstChild.nodeValue = this.formate_time(this.currentTime); //设置视频初始时间
+            this.initProcess();
+        };
+        //设置进度条长度
+        setVedioTime.prototype.initProcess = function() {
+            this.j_process_all = document.querySelector('.vedio-process');
+            this.j_process = document.querySelector('.vedio-process-status');
+            this.j_process_dot = document.querySelector('.vedio-process-dot');
 
+            var processWidth = 0; //进度条总长度
+            var dotwidth = this.j_process_dot.offsetWidth;
+            processWidth = (this.currentTime / this.vedioTime) * (this.j_process_all.offsetWidth - dotwidth); //进度条长度要减去点的宽度
+            //processWidth = this.j_process_all.offsetWidth;
+            this.setMovingDot(processWidth);
+        };
+        //各种操作
+        setVedioTime.prototype.initOpetaionFun = function() {
+            var that = this;
+            var moveBar = this.setMoveOpe();
+            //点击按钮开始或暂停视频
+            this.Jctrl.onclick = function() {
+                var bartimer;
+                if (that.Media.paused) {
+                    that.Media.play();
+                    this.className = "btn-control-play";
+                    bartimer = window.setInterval(function() {
+                        that.initTime();
+                    },
+                    100);
+                } else {
+                    that.Media.pause();
+                    this.className = "btn-control";
+                    window.clearInterval(bartimer);
+                }
+            };
+            this.j_process_dot.addEventListener("touchstart", moveBar.moveStart),
+            this.j_process_dot.addEventListener("touchmove", moveBar.moveIng),
+            this.j_process_dot.addEventListener("touchend", moveBar.moveEnd);
 
+        };
+        //拖动滑块设置播放时间
+        setVedioTime.prototype.setMoveOpe = function() {
+            var ope = {};
+            var that = this;
+            return ope = {
+                moveStart: function() {
 
+},
+                moveIng: function(e) {
+                    var X = e.touches[0].pageX;
+                    var precess_width = that.j_process_all.offsetWidth,
+                    treasure_width = that.j_process_dot.offsetWidth;
+                    X < -5 ? X = -5 : (X > precess_width ? X = precess_width - 5 : X > precess_width - treasure_width + 12 && (X = precess_width - treasure_width + 12)),
+                    that.space = X;
+                    var duration = parseInt(that.vedioTime),
+                    time = duration * X / parseInt(precess_width);
+                    that.currentTimeNode.firstChild.nodeValue = that.formate_time(time);
+                    that.setMovingDot(X);
+                },
+                moveEnd: function() {
+                    var precess_width = that.j_process_all.offsetWidth;
+                    var duration = parseInt(that.vedioTime),
+                    currentTime = duration * that.space / parseInt(precess_width - 5);
+                    console.log(that.vedioTime);
+                    that.Media.currentTime = currentTime;
+                }
+            };
+        };
+        //初始化函数
+        setVedioTime.prototype.initFinial = function() {
+            this.initTime();
+            this.initOpetaionFun();
+        };
 
+        var vedioPlay = new setVedioTime();
+        vedioPlay.initFinial();
+    })();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
